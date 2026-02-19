5f237dc1e5f2a6c579fd7ca1992e30af1789cf3d
Carlos Estrada <carabesle@gmail.com>
2026-02-18 21:33:05 -0600

    feat(user-service): add get and post methods

--- user-service/routes/main.go
+++ user-service/routes/main.go
@@ -4,6 +4,9 @@ import (
 	"time"
 
 	"github.com/caresle/microservices-workouts-tracker/shared"
+	"github.com/caresle/microservices-workouts-tracker/user-service/models"
+	"github.com/caresle/microservices-workouts-tracker/user-service/queries"
+	user_request "github.com/caresle/microservices-workouts-tracker/user-service/request"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,11 +19,74 @@ func Run() {
 }
 
 func getRoutes() {
-	v1 := router.Group("/api/v1")
+	v1 := router.Group("/api/v1/users")
 
 	v1.GET("/", func(ctx *gin.Context) {
+		users, err := queries.GetAllUsers()
+
+		if err != nil {
+			ctx.JSON(500, shared.ApiResponse{
+				Errors: []shared.APIError{
+					{
+						Code:    "INTERNAL_SERVER_ERROR",
+						Message: "Failed to get users",
+						Details: map[string]interface{}{
+							"error": err.Error(),
+						},
+					},
+				},
+				Timestamp: time.Now().Unix(),
+			})
+			return
+		}
+
 		ctx.JSON(200, shared.ApiResponse{
-			Data:      []any{"User service tracker"},
+			Data:      []any{users},
+			Timestamp: time.Now().Unix(),
+		})
+	})
+
+	v1.POST("/", func(ctx *gin.Context) {
+		var body user_request.CreateUserRequest
+
+		if err := ctx.ShouldBindJSON(&body); err != nil {
+			ctx.JSON(400, shared.ApiResponse{
+				Errors: []shared.APIError{
+					{
+						Code:    "VALIDATION_ERROR",
+						Message: "Invalid request body",
+						Field:   "body",
+						Details: map[string]interface{}{
+							"error": err.Error(),
+						},
+					},
+				},
+				Timestamp: time.Now().Unix(),
+			})
+			return
+		}
+
+		userBody := models.FromCreateRequestToUser(body)
+		user, err := queries.CreateUser(*userBody)
+
+		if err != nil {
+			ctx.JSON(500, shared.ApiResponse{
+				Errors: []shared.APIError{
+					{
+						Code:    "INTERNAL_SERVER_ERROR",
+						Message: "Failed to create user",
+						Details: map[string]interface{}{
+							"error": err.Error(),
+						},
+					},
+				},
+				Timestamp: time.Now().Unix(),
+			})
+			return
+		}
+
+		ctx.JSON(200, shared.ApiResponse{
+			Data:      []any{*user},
 			Timestamp: time.Now().Unix(),
 		})
 	})
